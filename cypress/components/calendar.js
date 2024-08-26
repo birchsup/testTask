9d5869d02248752e1aee9241a26d4743c47249d6
DmitrySado <168556229+DmitrySado@users.noreply.github.com>
2024-08-27 01:53:26 +0400

    little fix for ubuntu

--- cypress/components/calendar.js
+++ cypress/components/calendar.js
@@ -1,14 +1,14 @@
-class Calendar{
-    elements={
+class Calendar {
+    elements = {
         container: () => cy.get('.ticket-offer__calendar'),
-        dayLocator:('.event-calendar-day-number'),
-        monthSelectorLocator:(month) =>(`li[data-event-calendar-month-link="${month}"]`),
+        dayLocator: ('.event-calendar-day-number'),
+        monthSelectorLocator: (month) => (`li[data-event-calendar-month-link="${month}"]`),
         monthTable: (month) => (`table[data-event-calendar-table="${month}"]`),
-        timeOption: (time) =>('.flex-show-calendar-container .show-selection-time', time)
+        timeOption: (time) => ('.flex-show-calendar-container .show-selection-time', time)
     }
 
     selectDate(month, day, time) {
-        this.elements.container().find(this.elements.monthSelectorLocator(month)).click({ force: true });
+        this.elements.container().find(this.elements.monthSelectorLocator(month)).click({force: true});
         cy.get(this.elements.monthTable(month)).should('be.visible');
 
         cy.get(this.elements.monthTable(month))
@@ -17,11 +17,11 @@ class Calendar{
                 // day inside table
                 cy.get(this.elements.dayLocator)
                     .contains(day)
-                    .click({ force: true });
+                    .click({force: true});
                 cy.get(this.elements.dayLocator).contains(day).parent()
                     .should('have.css', 'background-color', 'rgb(104, 33, 130)');
-                cy.contains(this.elements.timeOption(time)).parent().should('be.visible');
-                cy.contains(this.elements.timeOption(time)).parent().click();
+                cy.contains(this.elements.timeOption(time)).should('be.visible');
+                cy.contains(this.elements.timeOption(time)).click();
             });
 
         //create onject
